40116d234616691db414b26be8d0dd48430952f3
Branko Radoš <branko.rados@fer.hr>
2017-06-15 16:11:37 +0200

    Gazebo change

--- src/robotName.sh
+++ src/robotName.sh
@@ -1,11 +1,5 @@
-export ROBOT_NAME=pioneer
 #!/usr/bin/env bash
-
-echo "Downloading image..."
-
-wget -O ../maps/white.png "https://user-images.githubusercontent.com/7688443/27013770-f5f55f38-4eea-11e7-8a4f-84f65c32858b.png"
-wget -O ../maps/maze.png "https://user-images.githubusercontent.com/7688443/27013774-0752e3a4-4eeb-11e7-993c-e2443e5c3d95.png"
+export ROBOT_NAME=pioneer
 
 
 
-echo "Done!"
